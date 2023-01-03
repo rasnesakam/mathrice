303790a330d655f87074855f7e221019a76241d1
ensar <ensar.makas@gmail.com>
2023-01-03 20:49:46 +0300

    added modules for different processes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
+pub mod types;
+pub mod ops;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
---
+++ src/test.rs
@@ -0,0 +1,12 @@
+mod types;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        
+        assert_eq!(result, 4);
+    }
+}
