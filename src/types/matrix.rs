4f92b460895aa6180992b254144875fce852e8f8
ensar <ensar.makas@gmail.com>
2023-01-03 20:49:15 +0300

    added implementations in types module

--- src/types/matrix.rs
+++ src/types/matrix.rs
@@ -1,5 +1,13 @@
-struct Matrix<T> {
-    row: i32,
-    col: i32,
-    data: [[T;col];row]
+use crate::types::Vector;
+
+pub struct Matrix<T> {
+    pub row: i32,
+    pub col: i32,
+    data: Vector<Vector>
+}
+
+impl <T> Matrix<T> {
+    pub fn get(&self, row: u64, col: u64) -> T {
+        self.data[row][col];
+    }
 }
--- src/types/vector.rs
+++ src/types/vector.rs
@@ -1,4 +1,13 @@
-struct Vector<T>{
-    size: u32,
-    data: [T;size]
+pub struct Vector<T>{
+    size: usize,
+    data: Box<[T]>
+}
+
+impl<T> Vector<T>{
+    fn new(datas: Box<[T]>)-> Vector<T>{
+        return Vector{
+            size: datas.len(),
+            data: datas
+        };
+    }
 }
