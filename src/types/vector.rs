e88af10f0ce227227c5be82901c11a84ac529e12
ensar <ensar.makas@gmail.com>
2023-03-04 23:25:10 +0300

    Cleared compile errors and handled sizing stuffs

--- src/types/vector.rs
+++ src/types/vector.rs
@@ -1,21 +1,29 @@
 use std::ops::Index;
 
-pub struct Vector<T>{
+#[derive(Clone, Copy)]
+pub struct Vector<const N: usize, T: Sized + Copy>{
     size: usize,
-    data: Box<[T]>
+    data: [Option<T>;N]
 }
 
-impl<T> Vector<T>{
-    fn new(datas: Box<[T]>)-> Vector<T>{
-        return Vector{
-            size: datas.len(),
-            data: datas
-        };
+
+impl<const N: usize, T: Sized + Copy> Vector<N, T>{
+    pub fn new(datas: [T;N])-> Vector<N, T>{
+        Vector{
+            size: N,
+            data: datas.map( | x | Some(x))
+        }
+    }
+    pub fn empty() -> Vector<N, T> {
+        Vector {
+            size: N,
+            data: [None;N]
+        }
     }
 }
 
-impl <T> Index<usize> for Vector<T> {
-	type Output = T;
+impl <const N: usize, T: Sized + Copy> Index<usize> for Vector<N, T> {
+	type Output = Option<T>;
 	fn index(&self, index: usize) -> &Self::Output {
 		return &self.data[index];
 	}
