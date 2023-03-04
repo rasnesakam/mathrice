e55adec6a8fd7bdbde22165e91854edcf6bc4d92
ensar <ensar.makas@gmail.com>
2023-03-04 23:24:54 +0300

    Cleared compile errors. And handled sizing stuffs

--- src/types/matrix.rs
+++ src/types/matrix.rs
@@ -2,35 +2,32 @@ use crate::types::vector::Vector;
 use crate::types::errors::IndexOutOfBoundError;
 use std::ops::Index;
 
-pub struct Matrix<T> {
+pub struct Matrix<const COL: usize, const ROW: usize, T: Sized + Copy> {
     pub row: usize,
     pub col: usize,
-    data: Vector<Vector<T>>
+    data: Vector<ROW, Vector<COL, T>>
 }
 
-impl <T> Matrix<T> {
-	pub fn new()
-    pub fn get(&self, row: usize, col: usize) -> Result<&T, IndexOutOfBoundError> {
+impl <const COL: usize, const ROW: usize, T: Sized + Copy> Matrix<COL, ROW, T> {
+	pub fn new(datas: [[T;COL];ROW]) -> Matrix<COL, ROW, T>{
+		let vector: Vector<ROW, Vector<COL, T>> = Vector::empty();
+		Matrix { row: ROW, col: COL, data: vector }
+	}
+    pub fn get(&self, row: usize, col: usize) -> Result<Option<T>, IndexOutOfBoundError> {
 		if row >= self.row {
 			return Err(IndexOutOfBoundError {message: format!("Cannot find row {}",row)});
 		}
 		if col > self.col {
 			return Err(IndexOutOfBoundError {message: format!("Cannot find column {}", col)});
 		}
-        return Ok(&self.data[row as usize][col as usize]);
+        return Ok(self.data[row].and_then(|data_col: Vector<COL, T>| data_col[col]));
     }
 }
 
-impl <T> Index<usize> for Matrix<T> {
-	type Output = Vector<T>;
+impl <const COL: usize, const ROW: usize, T: Sized + Copy> Index<usize> for Matrix<COL, ROW, T> {
+	type Output = Option<Vector<COL,T>>;
 	fn index(&self, index: usize) -> &Self::Output {
 		return &self.data[index];
 	}
 }
 
-impl <T> Index<(usize, usize)> for Matrix<T>{
-	type Output = T;
-	fn index(&self, index: (usize, usize)) -> &Self::Output {
-		return &self.data[index.0][index.1];
-	}
-}
