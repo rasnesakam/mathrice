abf58dc0b22b30d94aa48baaae946daeccca4428
ensar <ensar.makas@gmail.com>
2023-03-06 14:54:57 +0300

    update@test: Added new test case for vector sum

--- src/tests.rs
+++ src/tests.rs
@@ -6,3 +6,23 @@ fn it_works() {
     assert_eq!(vector[0].expect("EYVAH"), 4);
     assert_ne!(vector[1].expect("EYVAH"), 4)
 }
+
+#[test]
+fn vector_add_vector() {
+    let vec1: Vector<2, i32> = Vector::new([5, 6]);
+    let vec2: Vector<2, i32> = Vector::new([5, 4]);
+
+    let vec3:Vector<2, i32> = vec1 + vec2;
+
+    for i in 0..vec3.size {
+        
+        if vec3[i].is_some() {
+            let sum = vec1[i].unwrap() + vec2[i].unwrap();
+            let data = vec3[i].unwrap();
+            assert_eq!(data, sum )
+        }
+        else {
+            assert!(true, "Result is null at {}",i);
+        }
+    }
+}
