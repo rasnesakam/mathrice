b01737faf56a91204831fc0d3fa67fea2c786c6f
ensar <ensar.makas@gmail.com>
2023-03-19 15:29:25 +0300

    uptade@types/matrix: Updates are below...
    - Changed data type to arrays from Vector
    - Added some implementations for numeric and decimal values
    - Added some functions for getting elements and columns etc

--- src/types/matrix.rs
+++ src/types/matrix.rs
@@ -1,34 +1,136 @@
 use crate::types::vector::Vector;
 use crate::types::errors::IndexOutOfBoundError;
+use std::fmt::Display;
 use std::ops::Index;
 
-pub struct Matrix<const COL: usize, const ROW: usize, T: Sized + Copy> {
-    pub row: usize,
-    pub col: usize,
-    data: Vector<ROW, Vector<COL, T>>
+macro_rules! integral_numeric_matrix_impls {
+	($($t: ty)*) => ($(
+		impl <const ROW: usize> Matrix<ROW, ROW, $t>{
+			pub fn identity() ->Matrix<ROW, ROW, $t>{
+				let mut data: [[$t;ROW];ROW] = [[0;ROW];ROW];
+				for i in 0..ROW {
+					data[i][i] = 1;
+				}
+				Matrix {
+					row_size: ROW,
+					col_size: ROW,
+					data: data
+				}
+			}
+		}
+	)*);
 }
 
-impl <const COL: usize, const ROW: usize, T: Sized + Copy> Matrix<COL, ROW, T> {
-	pub fn new(datas: [[T;COL];ROW]) -> Matrix<COL, ROW, T>{
-		let mut vector: Vector<ROW, Vector<COL, T>> = Vector::empty();
-		for i in 0..datas.len() {
-			vector[i] = Some(Vector::new(datas[i]))
+macro_rules! decimal_numeric_matrix_impls {
+	($($t: ty)*) => ($(
+		impl <const ROW: usize> Matrix<ROW, ROW, $t>{
+			pub fn identity() ->Matrix<ROW, ROW, $t>{
+				let mut data: [[$t;ROW];ROW] = [[0.0;ROW];ROW];
+				for i in 0..ROW {
+					data[i][i] = 1.0;
+				}
+				Matrix {
+					row_size: ROW,
+					col_size: ROW,
+					data: data
+				}
+			}
 		}
-		Matrix { row: ROW, col: COL, data: vector }
+	)*);
+}
+
+
+pub struct Matrix<const ROW: usize, const COL: usize, T: Sized + Copy> {
+    pub row_size: usize,
+    pub col_size: usize,
+    data: [[T;COL];ROW]
+}
+
+integral_numeric_matrix_impls!(usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 );
+
+decimal_numeric_matrix_impls!(f32 f64);
+
+impl <const ROW: usize, const COL: usize, T> Matrix<ROW, COL, T>
+	where T: Sized + Copy{
+	pub fn with(item_suplier: fn() -> T ) -> Matrix<ROW, COL, T>{
+		//let vector: Vector<>
+		Matrix {
+			row_size: ROW,
+			col_size: COL,
+			data: [[item_suplier();COL];ROW]
+		}
 	}
-    pub fn get(&self, row: usize, col: usize) -> Result<Option<T>, IndexOutOfBoundError> {
-		if row >= self.row {
-			return Err(IndexOutOfBoundError {message: format!("Cannot find row {}",row)});
+	
+	pub fn fill_with(item_suplier: fn(row: usize, col: usize) -> T) -> Matrix<ROW, COL, T> {
+		let mut datas: [[T;COL];ROW] = [[item_suplier(0, 0);COL];ROW];
+		
+		for row in 0..ROW {
+			for col in 0..COL {
+				datas[row] = [item_suplier(row, col);COL];
+			}
 		}
-		if col > self.col {
-			return Err(IndexOutOfBoundError {message: format!("Cannot find column {}", col)});
+		Matrix {
+			row_size: ROW,
+			col_size: COL,
+			data: datas
 		}
-        return Ok(self.data[row].and_then(|data_col: Vector<COL, T>| data_col[col]));
-    }
+	}
 }
 
-impl <const COL: usize, const ROW: usize, T: Sized + Copy> Index<usize> for Matrix<COL, ROW, T> {
-	type Output = Option<Vector<COL,T>>;
+
+
+impl <const ROW: usize, const COL: usize, T> Matrix<ROW, COL, T> where
+	T: Sized + Copy{
+
+	pub fn get(&self, row_index: usize, col_index: usize) -> Result<T,IndexOutOfBoundError> {
+		if row_index < self.row_size {
+			if col_index < self.col_size {
+				return Ok(self.data[row_index][col_index])
+			}
+			return Err(IndexOutOfBoundError { 
+				message: format!("Index out of bounds for column ({}). requested: {}", self.col_size, col_index)
+			});
+		}
+		return Err(IndexOutOfBoundError { 
+			message: format!("Index out of bounds for row ({}). requested: {}", self.row_size, row_index )
+		});
+	}
+
+	pub fn get_col(&self, col_index: usize) -> Result<Matrix<ROW, 1, T>, IndexOutOfBoundError>{
+		if col_index >= self.col_size {
+            return Err(IndexOutOfBoundError { message: "Invalid column index".to_string() });
+        }
+		let mut datas: [[T;1];ROW] = [[self.data[0][col_index]];ROW];
+		for row in 0..ROW {
+			datas[row] = [self.data[row][col_index]];
+		}
+		Ok(Matrix {
+			row_size: ROW,
+			col_size: 1,
+			data: datas
+		})
+	}
+}
+
+impl <const ROW: usize, const COL: usize, T> Matrix<ROW, COL, T>
+	where T: Sized + Copy + Display{
+	pub fn print(&self) {
+		for i in 0..self.row_size {
+			print!("[ ");
+			for j in 0..self.col_size {
+				print!("{}",self.data[i][j]);
+				if j < self.col_size - 1 {
+					print!(", ")
+				}
+			}
+			println!(" ]");
+		}
+	}
+}
+
+
+impl <const COL: usize, const ROW: usize, T: Sized + Copy> Index<usize> for Matrix<ROW, COL, T> {
+	type Output = [T;COL];
 	fn index(&self, index: usize) -> &Self::Output {
 		return &self.data[index];
 	}
