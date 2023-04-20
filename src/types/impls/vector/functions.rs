cc0c0c128962d0274c310ff638aa934fd4236372
ensar <ensar.makas@gmail.com>
2023-04-20 14:27:24 +0300

    Added external functions

--- src/types/impls/vector/functions.rs
+++ src/types/impls/vector/functions.rs
@@ -1,11 +1,11 @@
 use crate::{types::Vector, errors::IndexOutOfBoundError};
 
 impl <const N: usize, T> Vector<N, T> {
-	pub fn get(&self, index: usize) -> Result<Option<T>, IndexOutOfBoundError> {
+	pub fn get(&self, index: usize) -> Result<&Option<T>, IndexOutOfBoundError> {
         if index > self.size {
             return Err(IndexOutOfBoundError { message: format!("Index out of bounds for {}",index) });
         }
-        Ok(self.data[index])
+        Ok(&self.data[index])
     }
 
     pub fn set(mut self, index: usize, data: T) {
