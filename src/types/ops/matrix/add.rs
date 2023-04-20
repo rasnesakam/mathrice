2a0d23d4a04530a6ad0dfc75b18c0a1b1ad345da
ensar <ensar.makas@gmail.com>
2023-04-20 14:28:51 +0300

    Completed addition function

--- src/types/ops/matrix/add.rs
+++ src/types/ops/matrix/add.rs
@@ -1,12 +1,21 @@
-use crate::types::matrix::Matrix;
+use crate::types::Matrix;
 use std::ops::Add;
 
-/*
 impl <const ROW: usize, const COL: usize, T> Add for Matrix<ROW,COL,T>
-    where T:Sized + Copy + Add{
+    where T:Sized + Copy + Add + Add<Output=T>{
     type Output = Matrix<ROW, COL, T>;
     fn add(self, rhs: Self) -> Self::Output {
-        let matrix : Matrix<COL,ROW, T>;
+		let mut data: [[T;COL];ROW] = self.data;
+		for i in 0..ROW {
+			for j in 0..COL {
+				data[i][j] = self[i][j] + rhs[i][j];
+			}
+		}
+
+		Matrix {
+			col_size: COL,
+			row_size: ROW,
+			data
+		}
     }
 }
-*/
