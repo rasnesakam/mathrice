ea175ade90560a8178b9ce137b5ca79f3e7691a8
Ensar Makas <ensar.makas@gmail.com>
2023-04-25 21:40:38 +0000

    Restrictions for T was unnecessary

--- src/types/impls/matrix/index.rs
+++ src/types/impls/matrix/index.rs
@@ -1,10 +1,9 @@
 use std::ops::Index;
-
 use crate::types::Matrix;
 
 
 
-impl <const COL: usize, const ROW: usize, T: Sized + Copy> Index<usize> for Matrix<ROW, COL, T> {
+impl <const COL: usize, const ROW: usize, T> Index<usize> for Matrix<ROW, COL, T> {
 	type Output = [T;COL];
 	fn index(&self, index: usize) -> &Self::Output {
 		return &self.data[index];
