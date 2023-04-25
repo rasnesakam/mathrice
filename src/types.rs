206f15884edcb2772d2d13c3b91c7e9375245b8f
Ensar Makas <ensar.makas@gmail.com>
2023-04-25 21:41:06 +0000

    Added new derive instructions

--- src/types.rs
+++ src/types.rs
@@ -2,14 +2,14 @@ pub mod impls;
 pub mod ops;
 
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub struct Matrix<const ROW: usize, const COL: usize, T> {
     pub row_size: usize,
     pub col_size: usize,
     data: [[T;COL];ROW]
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub struct Vector<const N: usize, T>{
     pub size: usize,
     data: [Option<T>;N]
