42bebc226596f039a900ca18bf48651934bb8096
ensar makas <ensar.makas@gmail.com>
2023-05-15 07:05:35 +0300

    added constructors for special matrices

--- src/types/impls/matrix/construct.rs
+++ src/types/impls/matrix/construct.rs
@@ -1,6 +1,5 @@
 use crate::types::Matrix;
 
-
 impl <const R: usize, const C: usize, T: Copy> Matrix<R,C,T> {
 	pub fn new(data: [[T;C];R]) -> Matrix<R,C,T> {
 		Matrix {
@@ -36,3 +35,20 @@ impl <const R: usize, const C: usize, T: Copy> Matrix<R,C,T> {
 		}
 	}
 }
+
+// numeric initializers
+
+macro_rules! numeric_instantiates {
+	($($ty: ty)*) => ($(
+		impl <const R: usize, const C: usize> Matrix<R,C,$ty> {
+			pub fn identity() -> Matrix<R,C,$ty> {
+				Matrix::fill_with(|r: usize, c: usize| if r == c {1.0 as $ty} else { 0.0 as $ty})
+			}
+			pub fn zero() -> Matrix<R,C,$ty> {
+				Matrix::fill_with_value(0.0 as $ty)
+			}
+		}
+	)*);
+}
+
+numeric_instantiates!(usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64);
